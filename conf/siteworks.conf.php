71c2190b200317b228e1c7bbfc6bd520bd8c06a5
chuck <chuck@chuckserver.com>
2019-03-31 22:10:00 +0000

    test removed

--- conf/siteworks.conf.php
+++ conf/siteworks.conf.php
@@ -5,8 +5,6 @@ if ( ! defined('SITEWORKS_DOCUMENT_ROOT')) exit('No direct script access allowed
 //    This is your main configuration file, if you want to make something cross server put it in here.
 //    If you want to override anything, put it in your individual server file.
 
-temp
-
 class siteworks_config extends siteworks_startup
 {
     /*********************************************************************************************************
