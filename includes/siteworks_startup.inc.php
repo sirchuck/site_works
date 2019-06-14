a08d9b183ee01f96920718e54e5a566dd43743a1
chuck <chuck@chuckserver.com>
2019-06-14 00:27:53 +0000

    unit testing fixes

--- includes/siteworks_startup.inc.php
+++ includes/siteworks_startup.inc.php
@@ -369,11 +369,11 @@ Start Time: " . date('Y-m-d H:i:s') . "
 			}
 
 			// U N I T    T E S T I N G
-			if( $this->_s->useUnitTests ){
+			if( $this->useUnitTests ){
 				$tmp='';$tmp2='';$tmp3='';$wf=false;$tc=0;$tt=[];
-				foreach($this->tool->listFiles(SITEWORKS_DOCUMENT_ROOT . '/dev/',1,true) as $v){
-					if( stripos(strrev($v['name']), 'php.') === 0 ){
-						preg_match_all('/(?<=\#_sw>)(.*?)(?=\n)|(?<nb>[^\{\}]*){0}(function.*?)(?<bb>\{\g<nb>\}|\{(?:\g<nb>\g<bb>\g<nb>)*\})/', file_get_contents($v['path'].$v['name']), $m);
+				foreach($this->tool->listFiles(SITEWORKS_DOCUMENT_ROOT . '/dev/',1,true) as $vf){
+					if( stripos(strrev($vf['name']), 'php.') === 0 ){
+						preg_match_all('/(?<=\#_sw>)(.*?)(?=\n)|(?<nb>[^\{\}]*){0}(function.*?)(?<bb>\{\g<nb>\}|\{(?:\g<nb>\g<bb>\g<nb>)*\})/', file_get_contents($vf['path'].$vf['name']), $m);
 						foreach($m[0] as $v){
 							if(strtolower(substr($v,0,8))=='function'){
 								if($wf){
@@ -390,7 +390,7 @@ Start Time: " . date('Y-m-d H:i:s') . "
 								// Store list of tests to run and trigger to store the next found function
 						        $wf = true;
 								$tt[$tc][0][] = trim($v);
-								$tt[$tc][4] = $v['path'].$v['name'];
+								$tt[$tc][4] = str_replace(SITEWORKS_DOCUMENT_ROOT . '/dev/','',$vf['path'].$vf['name']);
 							}
 						}
 					} // End if php file
@@ -399,28 +399,29 @@ Start Time: " . date('Y-m-d H:i:s') . "
 					// Itterate through every found test and function.
 					foreach($v[0] as $vv){
 						$vx = explode(',>',$vv); $vx2 = array_pop($vx);
-$tmp2 .= '$this->tool->dmsg("\nFile: '.$v[4].'\nFunction: '.$v[1].'\n");ob_start();unset($tmp);$tmp=$this->' . $v[1] . '(' . implode(',',$vx) . ');ob_end_clean();$this->_sw_valcheck("i",'.$vx2.', $tmp );'."\n";
+$tmp2.='$this->_tmp.="\nFile: '.$v[4].'\nFunction: '.$v[1].'\n";ob_start();unset($tmp);$tmp=$this->' . $v[1] . '(' . implode(',',$vx) . ');ob_end_clean();$this->_sw_valcheck("i",'.$vx2.', $tmp );'."\n";
 					}
 					$tmp3 .= $v[2]."\n";
 				}
+
+
 $xd = '<?php
 class _sw_unit_test {
-	private $_s = null;
+	private $_tmp = null;
 	private $_sw_test_counter = 0;
 	private $_sw_test_counter_fail = 0;
-    public function __construct(&s) {$_s = s;}
-    private function _sw_do_out($s){ $this->_s->tool->dmsg($s); }
+    public function __construct(){}
 	private function _sw_valcheck($t, $re, $rv){
 		$this->_sw_test_counter++;
 		if($re === $rv){
-			$_sw_do_out("Status: Success\n");
+			$this->_tmp .= "[c_light_blue]Status: Success[c_clear]\n";
 		}else if($re == $rv){
-			$_sw_do_out("Status: Type Fail\n");
-			$_sw_do_out("Expected: " . var_export($re, true) . "\nRecieved: " . var_export($rv, true) . "\n");
+			$this->_tmp .= "[c_light_red]Status: Type Fail[c_clear]\n";
+			$this->_tmp .= "Expected: " . var_export($re, true) . "\nRecieved: " . var_export($rv, true) . "\n";
 			$this->_sw_test_counter_fail++;
 		}else{
-			$_sw_do_out("Status: Fail\n");
-			$_sw_do_out("Expected: " . var_export($re, true) . "\nRecieved: " . var_export($rv, true) . "\n");
+			$this->_tmp .= "[c_red]Status: Fail[c_clear]\n";
+			$this->_tmp .= "Expected: " . var_export($re, true) . "\nRecieved: " . var_export($rv, true) . "\n";
 			$this->_sw_test_counter_fail++;
 		}
 	}
@@ -428,10 +429,15 @@ class _sw_unit_test {
 		try{
 '.$tmp2.'
 		}catch(Exception $e){
-			$_sw_do_out("Testing errror: ", $e->getMessage(), "\n");
+			$this->_tmp .= "Testing errror: " . $e->getMessage() . "\n";
 		}
-		$_sw_do_out("\nTotal Tests: " . $this->_sw_test_counter);
-		$_sw_do_out("\nTotal Failures: " . $this->_sw_test_counter_fail . "\n\n");
+		$this->_tmp .= "\nTotal Unit Tests: " . $this->_sw_test_counter;
+		if($this->_sw_test_counter_fail == 0){
+			$this->_tmp .= "\n[c_green]Total Unit Test Failures: " . $this->_sw_test_counter_fail . "[c_clear]\n\n";
+		}else{
+			$this->_tmp .= "\n[c_red]Total Unit Test Failures: [c_light_red]" . $this->_sw_test_counter_fail . "[c_clear]\n\n";
+		}
+		return $this->_tmp;
 	}
 '.$tmp3.'
 }
@@ -439,8 +445,10 @@ class _sw_unit_test {
 				$tfile = tmpfile();
 				fwrite($tfile, $xd);
 				require_once( stream_get_meta_data($tfile)['uri'] );
-				$xc = new _sw_unit_test($this);
-				$xc->_sw_dotest();
+				$xc = new \_sw_unit_test($this);
+				$debug_out .= "\n[c_cyan]----------- U N I T   T E S T I N G ----------[c_clear]\n";
+				$debug_out .= $xc->_sw_dotest();
+				$debug_out .= "[c_cyan]------------- END UNIT TESTING --------------[c_clear]\n\n";
 				fclose($tfile);
 				unset($xc);unset($xd);unset($tc);unset($tfile);unset($wf);unset($tt);
 			} // Allow Unit testing
